4d74fcb93cacf1b7a7325f62b6a7e875ceba07b1
snagurum <58689575+snagurum@users.noreply.github.com>
2024-09-10 01:00:33 +0000

    building presentation

--- qbank.js
+++ qbank.js
@@ -1,4 +1,5 @@
 /* meta data
+---=========================
 {
 -------------- given ------------
     no:                    1,
@@ -17,13 +18,12 @@
     LOG_LEVEL: "info",
     info: function(){
             if(log.LOG_LEVEL == "info")
-                 console.log(arguments.values());
+                 console.log(arguments);
 //               arguments.forEach(function(item,index){
 //                 console.log(item);
 //               });
           }
   };
-
 
   const shuffle = ( array ) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -33,12 +33,72 @@
     return array;
   };
 
-
   const shuffleOptions = ( array ) => {
     array.forEach(function(item, index){
       item.displayOptions = shuffle(item.options.concat(item.correctOptions));
     });
   }
 
+let QUESTION_BLOCK = ''                           +
+   '<div class="question ml-sm-5 pl-sm-5 pt-2">' +
+   '   <div class="py-2 h5">'+
+   '      <b>!@#$%___QUESTION_DATA___%$#@!</b>' +
+   '   </div>'+
+   '   !@#$%___OPTION_BLOCK___%$#@! '+
+   '</div>';
 
+let OPTION_BLOCK = ''+
+   '<div class="form-check">' +
+   '   <input class="form-check-input" type="checkbox" value="" id="option" />' +
+   '   <label class="form-check-label" for="flexCheckChecked">' +
+   '      !@#$%___OPTION_DATA___%$#@!' +
+   '   </label>'+
+   '</div>';
+
+  const getAnswers = (htmlElementClassname) => {
+    var array = [];
+    $(htmlElementClassname).each(function(){
+      array.push($(this).val());
+    });
+    return array;
+  }
+
+function getExam(){
+   return {
+      index: 0,
+      //questions:[]
+      getQuestionHtml: function(){
+           var displayOptionsHtml = '';
+           this.questions[this.index].displayOptions.forEach(function(item,index){
+              displayOptionsHtml = displayOptionsHtml + OPTION_BLOCK.
+                 replace('!@#$%___OPTION_DATA___%$#@!',item).
+                 replace('value=""','value="'+item+'" ');
+           });
+           return QUESTION_BLOCK.
+              replace('!@#$%___QUESTION_DATA___%$#@!',this.questions[this.index].question).
+              replace('!@#$%___OPTION_BLOCK___%$#@!' ,displayOptionsHtml);
+         },
+      prevQ: function(divId ){
+           if(this.index > 0){
+             this.index--;
+             $(divId ).empty();
+             $(divId ).append(this.getQuestionHtml());
+           }
+         },
+      nextQ: function(divId ){
+           this.answers=getAnswers('.form-check-input:checkbox:checked');
+           console.log('this.answers = ',this.answers);
+           if( this.index < this.questions.length-1){
+             this.index++;
+             $(divId ).empty();
+             $(divId ).append(this.getQuestionHtml());
+           }
+         },
+      logStatus:function(){
+           console.log('----------------------')
+           console.log('exam.index = ', this.index);
+           console.log('questoin = ', this.questions[exam.index]);
+         }
+   };
+}
 
